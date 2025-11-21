916ca125426953704fd7e1455cf022d97a91f83c
Mahir <mahirpatel1426@gmail.com>
2025-11-21 23:12:13 +0530

    env2

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -41,7 +41,7 @@ const Page = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className="bg-white shadow-xl rounded-xl p-8 w-full max-w-md">
         <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
-          Login User
+          Login user
         </h2>
 
         <form onSubmit={login} className="space-y-4">
